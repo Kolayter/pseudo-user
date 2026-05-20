7342dd2714739086b0b5ab02253de52ea91ecae4
Kolayter <kolayter5441@gmail.com>
2026-05-20 23:32:37 +0300

    Made fisherman.py

--- llm.py
+++ llm.py
@@ -7,7 +7,7 @@ load_dotenv()
 SERVER_IP = os.getenv("SERVER_IP")
 SERVER_IP_LOCAL = os.getenv("SERVER_IP_LOCAL")
 
-client = Client(host=SERVER_IP_LOCAL)
+client = Client(host=SERVER_IP)
 
 def TextToAI(messages, llm=LLM_MODEL, options=LLM_OPTIONS):
     response = client.chat(model=llm, messages=messages, options=options)
