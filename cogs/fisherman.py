9fccba882214281a6f76ee3a8b773e51207be00a
Kolayter <kolayter5441@gmail.com>
2026-05-28 14:39:23 +0300

    added abstractor [WIP]

--- cogs/fisherman.py
+++ cogs/fisherman.py
@@ -2,5 +2,17 @@ import asyncio
 import discord
 from discord.ext import commands
 
-async def on_message():
+class Events(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
         
+    @commands.Cog.listener()
+    async def on_message(message):
+        if message.author == self.bot.user:
+            return
+        
+        if bot.user.mentioned_in(message):
+            pass
+
+async def setup(bot):
+    await bot.add_cog(Fisherman(bot))
