3fa0d9c861da575bac2211d5cafd1a2e8e86e41e
Kolayter <kolayter5441@gmail.com>
2026-07-14 01:28:37 +0300

    I don't really know how to expand this

--- main.py
+++ main.py
@@ -5,40 +5,48 @@ import logging
 import discord
 from discord.ext import commands
 
-# >>>        My modules        <<<
-from modules.llm import text_to_ai
-from modules.event_manager import EventManager
-from modules.discord_stuff import DiscordEvents, DiscordOut
+# My modules
+import modules.discord_stuff
 from modules.logging_settings import setup_logging
-# >>>          Start           <<<
+
+
+# Start
 # .env stuff
 load_dotenv()
 DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")  
 
-# -------- Disocrd stuff ---------
+# Disocrd stuff
 bot = commands.Bot(command_prefix="!", intents=discord.Intents.all())
 # Damn, Discord spams all my console with DEBUG logs, I can't read it. There's the fix:
 logging.getLogger('discord').setLevel(logging.INFO)
 
-# -------- Logging stuff ---------
+#Logging stuff
 setup_logging()
 logger = logging.getLogger(__name__)
 
 
-# >>>   The main cycle logic   <<<
+
+
+
+# The main cycle logic
 async def main():
     logger.info(f"psuedo-user started!")
 
+    input_queue = asyncio.Queue()
+    output_queue - asyncio.Queue()
 
-    event_manager = EventManager()
-    discord_in = DiscordEvents(bot=bot, event_manager=event_manager)
-    discord_out = DiscordOut(bot=bot, event_manager=event_manager)
+    discord_in = DiscordIn(bot=bot, input_queue=input_queue)
+    discord_out = DiscordOut(bot=bot, output_queue=output_queue)
 
-    async with bot:
-        await bot.start(DISCORD_TOKEN)
+    await asyncio.gather(
+        # start the modules
+    )
 
 
+
+ 
 if __name__ == "__main__":
+    # Refactor to a real SAFE closing. 
     try:
         asyncio.run(main())
     except KeyboardInterrupt:
